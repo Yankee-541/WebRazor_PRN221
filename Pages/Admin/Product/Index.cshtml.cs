6b33fac5f03d91a9a27892ca9ea2f5e7aa47d6cd
D U Y Đ Ă N G <dangtd5401@gmail.com>
2022-10-17 00:32:14 +0700

    first commit

---
+++ Pages/Admin/Product/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebRazor.Models;
+
+namespace WebRazor.Pages.Admin.Product
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebRazor.Models.PRN221DBContext _context;
+
+        public IndexModel(WebRazor.Models.PRN221DBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Models.Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Product = (IList<Models.Product>)await _context.Products
+                .Include(p => p.Category).ToListAsync();
+            }
+        }
+    }
+}
