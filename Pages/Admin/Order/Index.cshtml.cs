3ab9249b4a526ac3cb4833c9e50359738a10c41b
D U Y Đ Ă N G <dangtd5401@gmail.com>
2022-10-17 17:44:30 +0700

    push code

---
+++ Pages/Admin/Order/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebRazor.Models;
+
+namespace WebRazor.Pages.Admin.Order
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebRazor.Models.PRN221DBContext _context;
+
+        public IndexModel(WebRazor.Models.PRN221DBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Models.Order> Order { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Orders != null)
+            {
+                Order = await _context.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Employee).ToListAsync();
+            }
+        }
+    }
+}
--- Pages/Admin/Product/Create.cshtml.cs
+++ Pages/Admin/Product/Create.cshtml.cs
@@ -1,12 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebRazor.Models;
 
-namespace WebRazor.Pages.Product
+namespace WebRazor.Pages.Admin.Product
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly WebRazor.Models.PRN221DBContext _context;
+
+        public CreateModel(WebRazor.Models.PRN221DBContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Models.Product Product { get; set; }
+
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
