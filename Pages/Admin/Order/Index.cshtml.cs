9392e99ab9325fcfb55ebcad83738eae1fcdafbc
D U Y Đ Ă N G <dangtd5401@gmail.com>
2022-10-18 13:32:07 +0700

    paging

--- Pages/Admin/Order/Index.cshtml.cs
+++ Pages/Admin/Order/Index.cshtml.cs
@@ -22,12 +22,34 @@ namespace WebRazor.Pages.Admin.Order
 
         public async Task OnGetAsync()
         {
+            int totalProduct = await _context.Products.CountAsync();
+            countPages = (int)Math.Ceiling((double)totalProduct / pageSize);
+
+            if (currentPage < 1)
+            {
+                currentPage = 1;
+            }
+            if (currentPage > countPages)
+            {
+                currentPage = countPages;
+            }
+
             if (_context.Orders != null)
             {
                 Order = await _context.Orders
                 .Include(o => o.Customer)
-                .Include(o => o.Employee).ToListAsync();
+                .Include(o => o.Employee).Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
             }
         }
+        public const int pageSize = 10;
+        [BindProperty(SupportsGet = true, Name = "currentPage")]
+        public int currentPage { get; set; }
+        public int countPages { get; set; }
+
+        public void OnPost(DateTime txtStartOrderDate, DateTime txtEndOrderDate)
+        {
+            Order = (from x in _context.Orders where (x.OrderDate >= txtStartOrderDate) && (x.OrderDate <= txtEndOrderDate) select x).ToList();
+        }
+
     }
 }
--- Pages/Admin/Product/Index.cshtml.cs
+++ Pages/Admin/Product/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebRazor.Models;
 
@@ -20,13 +21,37 @@ namespace WebRazor.Pages.Admin.Product
 
         public IList<Models.Product> Product { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string txtSearch)
         {
-            if (_context.Products != null)
+
+            int totalProduct = await _context.Products.CountAsync();
+            countPages = (int)Math.Ceiling((double)totalProduct / pageSize);
+
+            if (currentPage < 1)
             {
-                Product = (IList<Models.Product>)await _context.Products
-                .Include(p => p.Category).ToListAsync();
+                currentPage = 1;
             }
+            if (currentPage > countPages)
+            {
+                currentPage = countPages;
+            }
+
+            var qr = (from a in _context.Products orderby a.ProductId ascending select a).Skip((currentPage - 1) * pageSize).Take(pageSize);
+
+            if (!string.IsNullOrEmpty(txtSearch))
+            {
+                Product = qr.Where(a => a.ProductName.Contains(txtSearch)).ToList();
+            }
+            else
+            {
+                Product = await qr.ToListAsync();
+            }
+
         }
+
+        public const int pageSize = 10;
+        [BindProperty(SupportsGet = true, Name = "currentPage")]
+        public int currentPage { get; set; }
+        public int countPages { get; set; }
     }
 }
